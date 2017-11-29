b7205a95347c5a2b3895023bf6f0947af4c806e0
Megan Collier <meganraec@live.com>
2017-11-29 01:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,26 +5,21 @@ function add(a, b) {
   return a + b; 
 }
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+function subtract(a, b) {
+  return a - b; 
+}
 
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
+function multiply(a,b) {
+  return a * b; 
+}
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
+function divide(a, b) {
+  return a / b;
+}
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
+function increment(a,b) {
+  return 
+}
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
