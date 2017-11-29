534c15fab0a9e1d1034f15451fbbc24d3df74a7d
Megan Collier <meganraec@live.com>
2017-11-29 02:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ describe('makeInt(n)', function() {
   })
 })
 
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
